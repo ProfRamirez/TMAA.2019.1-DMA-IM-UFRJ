f26dffc0a5b89293c6d113a24c3fecd3768a88b1
ProfRamirez <48801344+ProfRamirez@users.noreply.github.com>
2019-07-09 16:08:11 -0300

    Update DataFrame.h
    inclui head

--- ExemplosCPP/ED04_2019.1/include/DataFrame.h
+++ ExemplosCPP/ED04_2019.1/include/DataFrame.h
@@ -1,3 +1,18 @@
+/*
+#-----------------------------------------------------------
+# UFRJ - Universidade Federal do Rio de Janeiro
+# IM   - Instituto de Matemática
+# DMA  - Departamento de Matemática Aplicada
+#
+#        TMAA - Top. Mat. Aplicada A - Programação I
+#
+# ED04 - Modulo DataFrame: Exploração da Base de Eleitores 2018
+#
+# Autor: Prof. Milton R. Ramirez (milton@labma.ufrj.br)
+# Criação: 07 de julho de 2019
+#-----------------------------------------------------------
+*/
+
 #ifndef DATAFRAME_H
 #define DATAFRAME_H
 #include "FuncaoAgregacao_SOMA.h"
@@ -14,7 +29,7 @@ class DataFrame
         void read_csv(string) {};
         DataFrame groupby(string) { return *this; };
         DataFrame agg( string, FuncaoAgregacao_SOMA ) { return *this; };
-        void print(void) {};
+        void Grafico(void) {};
 
     protected:
     private:
